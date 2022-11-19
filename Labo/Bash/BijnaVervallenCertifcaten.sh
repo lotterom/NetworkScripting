a5442ba629f20e4a39db30c0356b6853357ac7a1
lotterom <lotte.rombaut@student.howest.be>
2022-11-19 17:02:45 +0100

    verzamel alle certs ouder dan 14 dagen

--- Labo/Bash/BijnaVervallenCertifcaten.sh
+++ Labo/Bash/BijnaVervallenCertifcaten.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-#zoek alle certificaten (.crt)
-CERTS = find / -type f -name '*.crt'
+#zoek alle certificaten (.crt) ouder dan 2 weken
+CERTS = find / -type f -name '*.crt' -mtime +14
 
 #schrijf certificaten weg naar file
 FILE = /home/user/NetwScripting/certs/certificaten.txt
 CERTS >> FILE
 
+#overloop alle certificaten in bestand certificaten.txt en bekijke hun datum
+REGELS=$(cat $FILE)
+
+#for R in $REGELS
+#do
+#haal de datum uit de regels (https://www.cyberciti.biz/faq/find-check-tls-ssl-certificate-expiry-date-from-linux-unix/)
+    #DATUM=$(openssl x509 -enddate -noout -in $R)
+
+    #MAAND="$(echo "$DATUM" +%b)"
+    #DAG="$(echo "$DATUM" +%d)"
+    #JAAR="$(echo "$DATUM" +%Y)"
 
