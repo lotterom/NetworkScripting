16bccef82382dacb0d15b416fc687b743439d970
lotterom <lotte.rombaut@student.howest.be>
2022-11-19 16:31:51 +0100

    schrijf cert weg naar file

--- Labo/Bash/BijnaVervallenCertifcaten.sh
+++ Labo/Bash/BijnaVervallenCertifcaten.sh
@@ -2,3 +2,9 @@
 
 #zoek alle certificaten (.crt)
 CERTS = find / -type f -name '*.crt'
+
+#schrijf certificaten weg naar file
+FILE = /home/user/NetwScripting/certs/certificaten.txt
+CERTS >> FILE
+
+
